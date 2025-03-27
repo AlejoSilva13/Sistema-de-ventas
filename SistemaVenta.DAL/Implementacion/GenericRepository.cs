e7880072108ffb3a164a2ae57f4b4c90f269c44b
Alejandro Silva <dsilva_99@hotmail.com>
2025-03-27 17:20:01 -0500

    Implementacion de clases y interfaces

---
+++ SistemaVenta.DAL/Implementacion/GenericRepository.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+using SistemaVenta.DAL.DBCONTEXT;
+using SistemaVenta.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Implementacion
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+
+        private readonly DBVENTAContext _dbContext;
+
+        public GenericRepository(DBVENTAContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        #region Obtener
+        public async Task<TEntity> Obtener(Expression<Func<TEntity, bool>> filtro)
+        {
+            try
+            {
+                TEntity entidad = await _dbContext.Set<TEntity>().FirstOrDefaultAsync(filtro);
+                return entidad;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Crear
+
+        public async Task<TEntity> Crear(TEntity entidad)
+        {
+            try
+            {
+                _dbContext.Set<TEntity>().Add(entidad);
+                await _dbContext.SaveChangesAsync();
+                return entidad;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Editar
+
+        public async Task<bool> Editar(TEntity entidad)
+        {
+            try
+            {
+                _dbContext.Update(entidad);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Eliminar
+
+        public async Task<bool> Eliminar(TEntity entidad)
+        {
+            try
+            {
+                _dbContext.Remove(entidad);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Consultar
+
+        public async Task<IQueryable<TEntity>> Consultar(Expression<Func<TEntity, bool>> filtro = null)
+        {
+            try
+            {
+                IQueryable<TEntity> queryEntidad = filtro is null ? _dbContext.Set<TEntity>() : _dbContext.Set<TEntity>().Where(filtro);
+                return queryEntidad;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        #endregion
+
+
+    }
+}
+
---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DAL.DBCONTEXT;
+using Microsoft.EntityFrameworkCore;
+//using SistemaVenta.DAL.Interfaces;
+//using SistemaVenta.DAL.Implementacion;
+//using SistemaVenta.BLL.Interfaces;
+//using SistemaVenta.BLL.Implementacion;
+
+
+namespace SistemaVenta.IOC
+{
+	public static class Dependencia
+	{
+
+		public static void InyectarDependencia(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddDbContext<DBVENTAContext>(options =>
+			{
+				options.UseSqlServer(configuration.GetConnectionString("CadenaSQL"));
+			});
+		}
+	
+	}
+}
+
